64235a5604fa7873a555492efafe8d93db95eab3
Sabrina Nawaz <sabrinanawaz23@gmail.com>
2022-09-20 22:31:16 -0400

    Changed initials div to results and added variables for the questions, quiz and results to the script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-var btnStart = document.getElementById('btn-start');
+var timer = document.getElementById("timer");
+var btnStart = document.getElementById("btn-start");
+var quiz = document.getElementById("questions");
+var results = document.getElementById("results");
