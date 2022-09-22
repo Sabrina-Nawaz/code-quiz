2bfb446dadd677131554747f6a3be10bdfdfb1b7
Sabrina Nawaz <sabrinanawaz23@gmail.com>
2022-09-21 22:03:41 -0400

    Changed questions id in html and created p tags for questions and assigned text with textContent in script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 var timer = document.getElementById("timer");
 var btnStart = document.getElementById("btn-start");
-var quiz = document.getElementById("questions");
-var results = document.getElementById("results");
+var quizEl = document.getElementById("quiz");
+var resultsEl = document.getElementById("results");
+var timerStart;
+
+// Create Questions 
+var questionOne = document.createElement("p");
+var questionTwo = document.createElement("p");
+var questionThree = document.createElement("p");
+var questionFour = document.createElement("p");
+
+questionOne.textContent = "Commonly used data types do NOT include:"
+questionTwo.textContent = "The condition in an if/else statement is enclosed with: "
+questionThree.textContent = "Arrays in JavaScript can be used to store:"
+questionFour.textContent = "String values must be enclosed in what when being assigned to variables:"
